514156d6fa4a8d47de8145f02a35e693296cba31
Nevo.Mashiach <nevo.mashiach@appliedsystems.com>
2026-03-31 20:49:00 +0300

    add nevo-git formula

---
+++ Formula/nevo-git.rb
@@ -0,0 +1,27 @@
+class NevoGit < Formula
+  desc "Git aliases, branch helpers, and worktree management for zsh"
+  homepage "https://github.com/Nevo24/nevo-git"
+  url "https://github.com/Nevo24/nevo-git/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "f22393f66848be4d2825706b0709f311ec246ca1cca701147a8d8c45d371f0c5"
+  license "MIT"
+
+  def install
+    share.install "nevo-git.zsh" => "nevo-git/nevo-git.zsh"
+  end
+
+  def caveats
+    <<~EOS
+      Add this to your ~/.zshrc:
+
+        source "$(brew --prefix)/share/nevo-git/nevo-git.zsh"
+
+      Then run: source ~/.zshrc
+
+      Type `nevo-git` to see all available commands.
+    EOS
+  end
+
+  test do
+    assert_match "gits", shell_output("cat #{share}/nevo-git/nevo-git.zsh")
+  end
+end
