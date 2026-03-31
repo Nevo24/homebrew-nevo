a9ccbbe756737b4e2dc68640484e1efa361bc33c
Nevo.Mashiach <nevo.mashiach@appliedsystems.com>
2026-03-31 22:31:28 +0300

    bump to v0.1.13 (release)

--- Formula/nevo-git.rb
+++ Formula/nevo-git.rb
@@ -1,8 +1,8 @@
 class NevoGit < Formula
   desc "Git aliases, branch helpers, and worktree management for zsh"
   homepage "https://github.com/Nevo24/nevo-git"
-  url "https://github.com/Nevo24/nevo-git/archive/refs/tags/v0.1.12.tar.gz"
-  sha256 "e18d167efe00a84c750f6e5e436bc8389406192e808a66c49370be7bcf69dbcb"
+  url "https://github.com/Nevo24/nevo-git/archive/refs/tags/v0.1.13.tar.gz"
+  sha256 "4989b5cfa696672279969c72a173860f491e09b341bdbc46e7bf6c38ef58a114"
   license "MIT"
 
   def install
